e6e5d9c562a27dff740668d74bf5114ea22f6025
Chris Fowles <chris@prosus.io>
2017-12-12 13:06:55 +1100

    fully qualify output path

--- .build/Update-Capabilities.ps1
+++ .build/Update-Capabilities.ps1
@@ -1,19 +1,21 @@
+$DateString = (Get-Date).ToString('yyyyddMMHHmmss')
+
 if(($Env:AGENT_BUILDDIRECTORY)-and(Test-Path -Path $Env:AGENT_BUILDDIRECTORY)){
     $ModuleLocation = (Get-ChildItem -Path $ENV:AGENT_BUILDDIRECTORY -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName
+    $OutputPath = "$ENV:AGENT_BUILDDIRECTORY\Capabilities.$DateString.json"
 }
 else {
     $ModuleLocation = (Get-ChildItem -Path (Get-Item -Path $PSScriptRoot).Parent.FullName -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName    
+    $OutputPath = "$((Get-Item -Path $PSScriptRoot).Parent.FullName)\Capabilities.$DateString.json"
 }
+
+Write-Host "Output path is: $OutputPath"
+$Location = (Get-AzureRmLocation)[0].Location
+Write-Host "Using location: $Location"
 
 if (Test-Path $ModuleLocation -PathType Leaf){
     Write-Host "Importing module from: $ModuleLocation"
     Import-Module $ModuleLocation -Force    
-
-    $DateString = (Get-Date).ToString('yyyyddMMHHmmss')
-    $OutputPath = "Capabilities.$DateString.json"
-    Write-Host "Output path is: $OutputPath"
-    $Location = (Get-AzureRmLocation)[0].Location
-    Write-Host "Using location: $Location"
 
     Get-AzureRMCloudCapability `
         -Location $Location `
