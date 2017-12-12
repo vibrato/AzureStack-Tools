e26bc369f7ef0c0cd735749270588a04d866625c
Chris Fowles <chris@prosus.io>
2017-12-12 13:19:10 +1100

    export more variables

--- .build/Update-Capabilities.ps1
+++ .build/Update-Capabilities.ps1
@@ -3,6 +3,9 @@ $DateString = (Get-Date).ToString('yyyyddMMHHmmss')
 if(($Env:AGENT_BUILDDIRECTORY)-and(Test-Path -Path $Env:AGENT_BUILDDIRECTORY)){
     $ModuleLocation = (Get-ChildItem -Path $ENV:AGENT_BUILDDIRECTORY -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName
     $OutputPath = "$ENV:AGENT_BUILDDIRECTORY\Capabilities.$DateString.json"
+    
+    Write-Host "##vso[task.setvariable variable=OutputPath]$OutputPath"
+    Write-Host "##vso[task.setvariable variable=TemplateValidatorModule]$((Get-ChildItem -Path $ENV:AGENT_BUILDDIRECTORY -Filter 'AzureRM.TemplateValidator.psm1' -Recurse).FullName)"
 }
 else {
     $ModuleLocation = (Get-ChildItem -Path (Get-Item -Path $PSScriptRoot).Parent.FullName -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName    
@@ -21,8 +24,6 @@ if (Test-Path $ModuleLocation -PathType Leaf){
         -Location $Location `
         -OutputPath $OutputPath `
         -Verbose
-
-    Write-Host "##vso[task.setvariable variable=OutputPath]$OutputPath"
 }
 else {
     Write-Error "Unable to find module in location: $ModuleLocation"
