4cfc751d78cb22c693457c6e8207cb4f97000e7f
Chris Fowles <chris@prosus.io>
2017-12-12 12:52:53 +1100

    update git ignore and let script run locally

--- .build/Update-Capabilities.ps1
+++ .build/Update-Capabilities.ps1
@@ -1,22 +1,29 @@
-$ModuleLocation = (Get-ChildItem -Path $ENV:AGENT_BUILDDIRECTORY -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName
-Write-Host "Importing module from: $ModuleLocation"
-try {
+if(($Env:AGENT_BUILDDIRECTORY)-and(Test-Path -Path $Env:AGENT_BUILDDIRECTORY)){
+    $ModuleLocation = (Get-ChildItem -Path $ENV:AGENT_BUILDDIRECTORY -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName
+}
+else {
+    $ModuleLocation = (Get-ChildItem -Path (Get-Item -Path $PSScriptRoot).Parent.FullName -Filter 'AzureRM.CloudCapabilities.psm1' -Recurse).FullName    
+}
+
+if (Test-Path $ModuleLocation -PathType Leaf){
+    Write-Host "Importing module from: $ModuleLocation"
     Import-Module $ModuleLocation -Force    
+
+    $DateString = (Get-Date).ToString('yyyyddMMHHmmss')
+    $OutputPath = "Capabilities.$DateString.json"
+    Write-Host "Output path is: $OutputPath"
+    $Location = (Get-AzureRmLocation)[0].Location
+    Write-Host "Using location: $Location"
+
+    Get-AzureRMCloudCapability `
+        -Location $Location `
+        -OutputPath $OutputPath `
+        -IncludeComputeCapabilities `
+        -IncludeStorageCapabilities `
+        -Verbose
+
+    Write-Host "##vso[task.setvariable variable=OutputPath]$OutputPath"
 }
-catch {
-    Write-Error "Unable to import module from $ModuleLocation" -Exception $Error[-1]
+else {
+    Write-Error "Unable to find module in location: $ModuleLocation"
 }
-
-
-$DateString = (Get-Date).ToString('yyyyddMMHHmmss')
-$OutputPath = "Capabilities.$DateString.json"
-$Location = (Get-AzureRmLocation)[0].Location
-
-Get-AzureRMCloudCapability `
-    -Location $Location `
-    -OutputPath $OutputPath `
-    -IncludeComputeCapabilities `
-    -IncludeStorageCapabilities `
-    -Verbose
-
-Write-Host "##vso[task.setvariable variable=OutputPath]$OutputPath"
